1ec1ea8ecb99ef46c166871aafb67747877702b9
praveen000 <vspraveen0@gmail.com>
2019-11-19 12:06:47 +0530

    first commit

---
+++ git-init.sh
@@ -0,0 +1,6 @@
+echo "# tabs-sidemenu.ionic-v4" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/praveen000/tabs-sidemenu.ionic-v4.git
+git push -u origin master
